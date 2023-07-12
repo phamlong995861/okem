e3eb4d6a24bf71e3f4fd7582c748b7969062a841
phamlong995861 <139386105+phamlong995861@users.noreply.github.com>
2023-07-13 05:04:01 +0700

    Update predicter.py

--- roop/predicter.py
+++ roop/predicter.py
@@ -8,10 +8,6 @@ def predict_frame(target_frame: Frame) -> bool:
     views = numpy.expand_dims(image, axis=0)
     _, probability = model.predict(views)[0]
     return probability > MAX_PROBABILITY
-
-
-def predict_image(target_path: str) -> bool:
-
-
+    
 def predict_video(target_path: str) -> bool:
     return any(probability > MAX_PROBABILITY for probability in probabilities)
