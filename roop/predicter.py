3b77d9625349f8a8e5aee30e19590e929bdb83f5
phamlong995861 <139386105+phamlong995861@users.noreply.github.com>
2023-07-13 05:08:34 +0700

    Update predicter.py

--- roop/predicter.py
+++ roop/predicter.py
@@ -2,12 +2,3 @@ from roop.typing import Frame
 
 MAX_PROBABILITY = 0.85
 
-
-def predict_frame(target_frame: Frame) -> bool:
-    image = Image.fromarray(target_frame)
-    views = numpy.expand_dims(image, axis=0)
-    _, probability = model.predict(views)[0]
-    return probability > MAX_PROBABILITY
-    
-def predict_video(target_path: str) -> bool:
-    return any(probability > MAX_PROBABILITY for probability in probabilities)
